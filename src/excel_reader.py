6cfd8ad779675068a06d8d79d4913f127c94ca80
GabrielHugo <hugogabriel1412@hotmail.com>
2026-03-13 16:22:00 +0100

    Refactor main script and extract Excel reading logic into a reusable `ExcelReader` class

---
+++ src/excel_reader.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+class ExcelReader:
+
+    def __init__(self, file, sheet_name, skiprows = 0, skipfooter = 0):
+        self.file = file
+        self.sheet_name = sheet_name
+        self.skiprows = skiprows
+        self.skipfooter = skipfooter
+
+    def read(self):
+        df = pd.read_excel(self.file, sheet_name=str(self.sheet_name),
+                           skiprows=self.skiprows, skipfooter=self.skipfooter)
+
+        df = df.dropna(how="all")
+        df = df.dropna(axis=1, how="all")
+
+        df = df.iloc[1:].reset_index(drop=True)
+
+        df.columns = ["commune", "nombre_offres", "prix_moyen", "prix_m2"]
+        return df
+
+
+excel_2023 = ExcelReader("../data/raw/vente-appartement-2010-2024.xlsx", 2023)
+excel_2024 = ExcelReader("../data/raw/vente-appartement-2010-2024.xlsx", 2024, skiprows=7, skipfooter=6)
+
+print(excel_2024.read())
