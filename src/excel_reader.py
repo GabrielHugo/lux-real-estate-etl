c68ba9adcf936be2bdc845ca6b4f3447bfb50148
GabrielHugo <hugogabriel1412@hotmail.com>
2026-03-14 15:39:13 +0100

    Refactor `ExcelReader` to handle multi-sheet processing and update main script accordingly

--- src/excel_reader.py
+++ src/excel_reader.py
@@ -1,29 +1,62 @@
 import pandas as pd
 import numpy as np
 
-
 class ExcelReader:
 
-    def __init__(self, file, sheet_name, skiprows = 7, skipfooter = 6):
+    def __init__(self, file, sheet_name = 2020, skiprows = 7, skipfooter = 6):
+
         self.file = file
         self.sheet_name = sheet_name
         self.skiprows = skiprows
         self.skipfooter = skipfooter
 
-        if self.sheet_name < 2021:
-            self.skiprows = 10
+    def get_all_sheet_names(self):
 
-    def read(self):
+        df = pd.ExcelFile(self.file)
 
-        df = pd.read_excel(self.file, sheet_name=str(self.sheet_name),
-                           skiprows=self.skiprows, skipfooter=self.skipfooter)
+        all_years = df.sheet_names
 
-        df = df.replace(to_replace="*", value=np.nan)
+        all_years = list(map(int, all_years))
 
-        df = df.dropna(how="all")
-        df = df.dropna(axis=1, how="all")
+        return all_years
 
-        df = df.iloc[1:].reset_index(drop=True)
 
-        df.columns = ["commune", "nombre_offres", "prix_moyen", "prix_m2"]
-        return df
+    def extract_and_transform(self):
+
+        all_sheet_names = ExcelReader(self.file)
+
+        years = all_sheet_names.get_all_sheet_names()
+
+        for year in years:
+
+            if year < 2021:
+
+                df = pd.read_excel(self.file, sheet_name=str(year),
+                                   skiprows= 10, skipfooter=self.skipfooter)
+
+                df = df.replace(to_replace="*", value=np.nan)
+
+                df = df.dropna(how="all")
+                df = df.dropna(axis=1, how="all")
+
+                df = df.iloc[1:].reset_index(drop=True)
+
+                df.columns = ["commune", "nombre_offres", "prix_moyen", "prix_m2"]
+
+            else:
+
+                df = pd.read_excel(self.file, sheet_name=str(year),
+                                   skiprows=7, skipfooter=self.skipfooter)
+
+                df = df.replace(to_replace="*", value=np.nan)
+
+                df = df.dropna(how="all")
+                df = df.dropna(axis=1, how="all")
+
+                df = df.iloc[1:].reset_index(drop=True)
+
+                df.columns = ["commune", "nombre_offres", "prix_moyen", "prix_m2"]
+
+
+
+            print(df, year)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from excel_reader import ExcelReader
 
-df = ExcelReader("../data/raw/vente-appartement-2010-2024.xlsx",
-                 2020, 7, 6)
+# ExcelReader
 
-print(df.read().isna().sum())
+df = ExcelReader("../data/raw/vente-appartement-2010-2024.xlsx")
+
+df.extract_and_transform()
